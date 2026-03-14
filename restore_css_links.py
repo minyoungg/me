2f5fb32acb23c8859a8c4e0d279753b630466405
min <minyoungg@gmail.com>
2026-03-13 21:03:20 -0700

    update

---
+++ restore_css_links.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+p = Path('/home/min/code/me/index.html')
+s = p.read_text()
+old = '    <link rel="stylesheet" type="text/css" href="stylesheet.css">\n    <style>\n'
+new = '    <link rel="stylesheet" type="text/css" href="stylesheet.css">\n    <link rel="stylesheet" href="css/ionicons.min.css">\n    <link rel="stylesheet" href="css/style.css">\n    <style>\n'
+p.write_text(s.replace(old, new, 1))
